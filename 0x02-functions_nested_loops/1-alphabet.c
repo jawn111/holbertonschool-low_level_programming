2f4b2c575edf49428a36754d79d6a59053c1388b
jawn111 <jawn111@gmail.com>
2023-01-30 19:02:17 -0800

    0x02-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_alphabet - | starts file
+ *
+ * Return: - | ends file
+ */
+
+void print_alphabet(void)
+{
+
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+
+		_putchar(x);
+
+	}
+
+	_putchar('\n');
+
+}
