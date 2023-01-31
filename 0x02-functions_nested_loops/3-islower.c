d56207f05b5ab13cd29c97ecc10618354a06c428
jawn111 <jawn111@gmail.com>
2023-01-30 19:15:57 -0800

    0x02-3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _islower - starts file
+ * @c: - int
+ * Return: - ends file
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
