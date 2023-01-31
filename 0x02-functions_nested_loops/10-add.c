a60e116073a8c43f27e25406867f0829eaf3bda7
jawn111 <jawn111@gmail.com>
2023-01-30 19:42:30 -0800

    0x02-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add- adds two integers
+ * @x: int variable one
+ * @y: int variable two
+ * Return: int
+ */
+
+int add(int x, int y)
+{
+		int f = (x + y);
+
+			return (f);
+}
