feae5bd4d4a108de563bb180dda732961f063b3e
jawn111 <jawn111@gmail.com>
2023-01-30 17:50:26 -0800

    0x01-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- prints the alphabet in lower case
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
