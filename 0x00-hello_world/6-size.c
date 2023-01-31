cb50c1f29baf8174b079fcdb8bab00e2a9de0f2e
jawn111 <jawn111@gmail.com>
2023-01-31 12:13:18 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,18 @@
 #include <string.h>
 /**
  * main - entry point
+ * @u: unsigned long
  * Return: Always 0
  */
 
 int main(void)
 {
 
-	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of an int: %i bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 
 	return (0);
 
