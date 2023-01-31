adb9b8753b2c74dc0a7c829a086d1abddd9912f4
jawn111 <jawn111@gmail.com>
2023-01-30 18:57:42 -0800

    0x00-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - | starts file
+ *
+ * Return: - | ends file
+ */
+
+int main(void)
+{
+
+	printf("_putchar");
+
+	putchar('\n');
+
+	return (0);
+
+}
