e8675ab61aa22fd93b0f374073d5543ce4fc27f0
jawn111 <jawn111@gmail.com>
2023-01-30 19:25:42 -0800

    0x02-5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,33 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
 /**
- * print_sign - function to print sign of a number
- * @n: First parameter
- * Return: 1 and prints + if n > 0, ret 0/print0 if n = 0, ret -1/prnt- in n < 0
+ * print_sign - | starts file
+ * @n: - | int
+ * Return: - | ends file
  */
 
 int print_sign(int n)
 {
-		if (n > 0)
-		{
-			_putchar('+');
-			return (1);
-		}
-		else if (n == 0)
-			{
-				_putchar('0');
-				return (0);
-			}
-			else
-				{
-					_putchar('-');
-					return (-1);
-				}
-				return (n);
+
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+
+		return (-1);
+	}
 }
